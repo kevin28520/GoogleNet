d94f5eb1a77fc346c6f4504783c0269a49d6a862
Guoqing Xu <kevin28520@gmail.com>
2019-02-01 17:17:02 -0600

    Update plot_prediction.py

--- plot_prediction.py
+++ plot_prediction.py
@@ -1,21 +1,25 @@
 # horizontal bar to show predictions
-def plot_prob(prob, name):
+def barplot_prob(prob, name):
+  # prob: np.ndarray, 1xn
+  # name: np.ndarray, 1xn
+  
+  assert prob.size == name.size
+  n_name = prob.size
+  
   arg_sort = prob.argsort()
+  
+  name = name[0, arg_sort]
+  name = np.reshape(name, (n_name, ))
 
   p_sorted = prob[0, arg_sort]
-  p_sorted = np.reshape(p_sorted, (20, ))
-
-  name = np.array(train_all.target_names)
-  name = name[arg_sort]
-  name = np.reshape(name, (20, ))
+  p_sorted = np.reshape(p_sorted, (n_name, ))
 
   plt.figure(figsize=(12, 9))
-  plt.barh(np.arange(20), p_sorted)
+  plt.barh(np.arange(prob.size), p_sorted)
 
   plt.xlim([0., 1.1])
   plt.xlabel('probability')
   plt.ylabel('class')
-
-  plt.yticks(np.arange(20), name)
+  plt.yticks(np.arange(n_name), name)
 
   plt.show()
